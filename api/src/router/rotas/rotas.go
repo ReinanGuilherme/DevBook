ff87391443348d7aa005ea6d1037451fd6730717
Reinan Guilherme de Oliveira Neto <reinan.neto@grupofasitec.local>
2022-11-24 13:18:43 -0300

    first commit

---
+++ api/src/router/rotas/rotas.go
@@ -0,0 +1,28 @@
+package rotas
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// rota representa todas as rotas da API
+type Rota struct {
+	URI                string
+	Metodo             string
+	Funcao             func(http.ResponseWriter, *http.Request)
+	RequerAutenticacao bool
+}
+
+// configurar, coloca todas as rotas dentro do router
+func Configurar(r *mux.Router) *mux.Router {
+	//pegando o slice das rotasUsuarios
+	rotas := rotasUsuarios
+
+	for _, rota := range rotas {
+		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo)
+	}
+
+	return r
+
+}
